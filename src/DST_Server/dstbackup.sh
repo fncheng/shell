ed5959719fb5884aa0159acac4157fc16acde757
fncheng <dong862647669@gmail.com>
2020-08-21 22:19:41 +0800

    更新备份命令

--- src/DST_Server/dstbackup.sh
+++ src/DST_Server/dstbackup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#while(sleep 86400)
-#do
+#$1:输出压缩包名称,日期格式如:0821
 DST=~/.klei/DoNotStarveTogether
 DATE=`date '+%m%d'`
 
+#删除dstbackup目录,再新建该目录,复制存档至该目录,打包压缩该目录
 rm -rf $DST/dstbackup
 mkdir $DST/dstbackup
 cp -af $DST/MyDediServer/{Caves,Master}  $DST/dstbackup
-zip -qr /www/wwwroot/cheng/dst/dstbackup$DATE.zip $DST/dstbackup
+tar -zcvf /www/wwwroot/cheng/dst/dstbackup$1.tar.gz $/DST/dstbackup/{Caves,Master}
+#zip -qr /www/wwwroot/cheng/dst/dstbackup$DATE.zip $DST/dstbackup
