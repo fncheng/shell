6a03b7b3573008f350025907a54bd1561b79e8eb
fncheng <dong862647669@gmail.com>
2020-08-21 22:23:05 +0800

    更新存档恢复命令

--- src/DST_Server/dstretreated.sh
+++ src/DST_Server/dstretreated.sh
@@ -2,5 +2,7 @@
 #DST:dst directory
 DST=~/.klei/DoNotStarveTogether
 
+#删除存档,解压备份存档至dstbackup,复制dstbackup至存档目录
 rm -rf $DST/MyDediServer/{Caves,Master}
+tar -zxvf /www/wwwroot/cheng/dst/dstbackup$1.tar.gz
 cp -af $DST/dstbackup/{Caves,Master} $DST/MyDediServer
