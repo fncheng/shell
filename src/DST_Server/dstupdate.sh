74ab2d15b9fd38a745d4b2f2909f963f780adb12
fncheng <dong862647669@gmail.com>
2020-08-22 10:17:03 +0800

    Update

--- src/DST_Server/dstupdate.sh
+++ src/DST_Server/dstupdate.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 #quit master dst server and update server
 screen -dr master -X quit
+screen_name="master"
 /home/steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/dstserver +app_update 343050 +quit
 sleep 10
-screen -S master
-./run_dedicated_servers.sh
+screen -dmS $screen_name
+cmd="./run_dedicated_servers.sh"
+#向screen发送$cmd命令
+screen -X -S $screen_name -p 0 -X stuff $cmd
+screen -x -S $screen_name -p 0 -X stuff $'\n'
