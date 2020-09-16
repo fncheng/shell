1a53a224d909e13703c3e0bf2e319f8ec5bcf59c
fncheng <dong862647669@gmail.com>
2020-09-16 21:32:14 +0800

    Update: 改用-C 指定相对路径

--- src/DST_Server/dstbackup.sh
+++ src/DST_Server/dstbackup.sh
@@ -8,6 +8,6 @@ rm -rf $DST/dstbackup
 mkdir $DST/dstbackup
 cp -af $DST/MyDediServer/{Caves,Master}  $DST/dstbackup
 #判断$1是否存在,$1为shell传入的第一个变量,存在即手动备份,不存在自动备份
-if [ $1 ]; then tar -zcvf /www/wwwroot/cheng/dst/dstbackup$1.tar.gz $DST/dstbackup/{Caves,Master}
-else tar -zcvf /www/wwwroot/cheng/dst/dstbackup$DATE.tar.gz $DST/dstbackup/{Caves,Master}; fi
+if [ $1 ]; then tar -zcvf /www/wwwroot/cheng/dst/dstbackup$1.tar.gz -C $DST/dstbackup/ {Caves,Master}
+else tar -zcvf /www/wwwroot/cheng/dst/dstbackup$DATE.tar.gz -C $DST/dstbackup/ {Caves,Master}; fi
 #zip -qr /www/wwwroot/cheng/dst/dstbackup$DATE.zip $DST/dstbackup
--- src/DST_Server/dstretreated.sh
+++ src/DST_Server/dstretreated.sh
@@ -4,5 +4,5 @@ DST=~/.klei/DoNotStarveTogether
 
 #删除存档,解压备份存档至dstbackup,复制dstbackup至存档目录
 rm -rf $DST/MyDediServer/{Caves,Master}
-if [ $1 ]; then tar -zxvf /www/wwwroot/cheng/dst/dstbackup$1.tar.gz && cp -af $DST/dstbackup/{Caves,Master} $DST/MyDediServer
+if [ $1 ]; then tar -zxvf /www/wwwroot/cheng/dst/dstbackup$1.tar.gz -C $DST/dstbackup/ && cp -af $DST/dstbackup/{Caves,Master} $DST/MyDediServer
 else cp -af $DST/dstbackup/{Caves,Master} $DST/MyDediServer; fi
